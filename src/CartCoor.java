a4b47c76774275d3326f6cd582b57a076128c6c2
Cezary Slotwinski <seezar@o2.pl>
2018-07-16 09:01:50 +0200

    Poprawka 16.07: w klasie CartCoor

--- src/CartCoor.java
+++ src/CartCoor.java
@@ -17,6 +17,14 @@ public class CartCoor {
             System.out.println("Punkt (" + x + "," + y + ") leży w III ćwiartce układu współrzędnych.");
         } else if (x < 0 && y > 0) {
             System.out.println("Punkt (" + x + "," + y + ") leży w IV ćwiartce układu współrzędnych.");
+        } else if (x == 0 && y > 0) {
+            System.out.println("Punkt (" + x + "," + y + ") leży na dodatniej części osi Y.");
+        } else if (x == 0 && y < 0) {
+            System.out.println("Punkt (" + x + "," + y + ") leży na ujemnej części osi Y.");
+        } else if (x > 0 && y == 0) {
+            System.out.println("Punkt (" + x + "," + y + ") leży na dodatniej części osi X.");
+        } else if (x < 0 && y == 0) {
+            System.out.println("Punkt (" + x + "," + y + ") leży na ujemnej części osi X.");
         } else {
             System.out.println("Wybrałeś środek układu współrzędnych.");
         }
